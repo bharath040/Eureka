789c337cfd59077d7af2c7909c9b7f8081c5f30a
bharath040 <bharathatkuri@gmail.com>
2017-11-20 01:45:45 -0600

    committing three projects

---
+++ hello-server/src/main/java/com/techprimers/cloud/helloserver/HelloServerApplication.java
@@ -0,0 +1,12 @@
+package com.techprimers.cloud.helloserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServerApplication.class, args);
+	}
+}
